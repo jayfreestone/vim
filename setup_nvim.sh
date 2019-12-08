692fc82932c2d1b918d515bc0525fac6176580d1
Jay Freestone <mail@jayfreestone.com>
2019-12-08 19:54:00 +0000

    Add example neovim setup script.

---
+++ setup_nvim.sh
@@ -0,0 +1,6 @@
+# https://thoughtbot.com/blog/my-life-with-neovim
+mkdir -p ~/.config
+ln -s ~/.vim ~/.config/nvim
+
+# https://github.com/deoplete-plugins/deoplete-jedi/wiki/Setting-up-Python-for-Neovim#simple-setup
+pip3 install --user neovim
